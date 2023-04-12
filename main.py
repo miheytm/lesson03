17bfd9a27061f8b940193bc99d0795b3de430af6
Mikhail Turasov <miheytm@mail.ru>
2023-04-12 17:37:11 +0500

    Решили Задача 16:

---
+++ main.py
@@ -0,0 +1,15 @@
+#Задача 16:
+
+import random
+list_1 = []
+n = int(input('Введите количество элементов: '))
+for i in range(n):
+        list_1.append(random.randint(-20,20))
+print(list_1)
+x = int(input('Введите элемент для проверки: '))
+print('Количество вхождений: ',list_1.count(x))
+#Задача 18:
+
+
+#*Задача 20
+
